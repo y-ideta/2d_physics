3f66412ea880fbc7f4552343f50e0af09323b1bf
y-ideta <yuki@ideta.dev>
2020-05-13 15:05:34 +0900

    add comment

--- simu.cpp
+++ simu.cpp
@@ -17,13 +17,13 @@ void calc(Obj& obj) {
     int w = glutGet(GLUT_WINDOW_WIDTH);
     int h = glutGet(GLUT_WINDOW_HEIGHT);
 
-    if ((obj.y+obj.size) >= h) {
+    if ((obj.y+obj.size) >= h) { // 床に当たった時
         float diff = h - (obj.y + obj.size);
         obj.vy = E_NUM * -obj.vy;
         obj.y = h - obj.size + diff; // diffを足してやらないとはみ出た分が意図しない損失になる
     }
 
-    if ((obj.y-obj.size) <= 0) {
+    if ((obj.y-obj.size) <= 0) { // 天井に当たった時
         float diff = obj.y - obj.size;
         obj.vy = E_NUM * -obj.vy;
         obj.y = obj.size - diff;
